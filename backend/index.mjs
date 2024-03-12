937b9c531aab3d7939f8e3711eebad39fb6f598f
Muneeb-Babar <muneeb63ahmad63@gmail.com>
2024-03-12 16:32:06 +0500

    Update

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import cros from 'cors'
 import routes from './routes/index.mjs'
 import db from './config/db.mjs'
 import {PORT} from './config/enviroment.mjs'
@@ -10,6 +11,7 @@ db.connection.once('open', () => console.log("connected to db")).on("error", (er
 app.listen(PORT, function () {
     console.log("Server is ready to use");
 });
+app.use(cros())
 app.use(express.json())
 //GET, POST, PUT, DELETE
 app.use('/', routes)
