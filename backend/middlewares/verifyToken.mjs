e56b62d3498685826f9532c95783d10c583fa84b
Muneeb-Babar <muneeb63ahmad63@gmail.com>
2024-03-16 16:42:05 +0500

    update

--- backend/middlewares/verifyToken.mjs
+++ backend/middlewares/verifyToken.mjs
@@ -1,34 +1,38 @@
-import jwt from 'jsonwebtoken'
-import Users from '../models/Users.mjs'
-import jwtSecret from '../config/jwt.mjs'
+import jwt from 'jsonwebtoken';
+import Users from '../models/Users.mjs';
+import jwtSecret from '../config/jwt.mjs';
 
-
-async function verifyToken(req,res,next){
-    const token = req.headers.authorization?.slice(7)
-    console.log('token',token)
-
-    if (!token) {
-        res.status(401).send({ message: "No access!" })
-        return
-    }
-
+async function verifyToken(req, res, next) {
     try {
+        // Extract token from the Authorization header
+        const token = req.headers.authorization?.slice(7);
+
+        // If token is missing, respond with 401 Unauthorized
+        if (!token) {
+            return res.status(401).send({ message: "No access!" });
+        }
+
+        // Verify the token and decode its payload
         const decoded = jwt.verify(token, jwtSecret);
 
-        const tokenExists = await Users.findOne({ tokens: token })
+        // Check if the token exists in the database
+        const tokenExists = await Users.exists({ tokens: token });
 
+        // If token doesn't exist, respond with 401 Unauthorized
         if (!tokenExists) {
-            res.status(401).send({ message: "Invalid token!" })
-            return
+            return res.status(401).send({ message: "Invalid token!" });
         }
-        
-        req.userId = decoded._id
-        req.tokenToRemove = token
 
-        next()
+        // Attach user ID and token to the request object
+        req.userId = decoded._id;
+        req.tokenToRemove = token;
+
+        // Call the next middleware or route handler
+        next();
     } catch (e) {
-        res.status(401).send({ message: "Invalid token!" })
+        // If token verification fails, respond with 401 Unauthorized
+        res.status(401).send({ message: "Invalid token!" });
     }
 }
 
-export default verifyToken
+export default verifyToken;
